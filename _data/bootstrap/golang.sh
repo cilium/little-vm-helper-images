f6b06796036f892809ada65deb00a44e3961a336
Tobias Klauser <tobias@cilium.io>
2023-08-31 11:21:20 +0200

    Let renovate update Go version
    Keep the Go version in the images up to date using renovate.
    
    Unfortunately, there is no way to get the tarball sha using the
    golang-version datasource in renovate, so let's omit it for now.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- _data/bootstrap/golang.sh
+++ _data/bootstrap/golang.sh
@@ -3,11 +3,10 @@ set -euxo pipefail
 
 . /etc/profile
 
+# renovate: datasource=golang-version depName=go
 GOLANG_VERSION=1.20.1
 GOLANG_DOWNLOAD_URL=https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz
-GOLANG_DOWNLOAD_SHA256=000a5b1fca4f75895f78befeb2eecf10bfff3c428597f3f1e69133b63b911b02
 
 curl -fsSL "$GOLANG_DOWNLOAD_URL" -o golang.tar.gz \
-    && echo "$GOLANG_DOWNLOAD_SHA256  golang.tar.gz" | sha256sum -c - \
     && tar -C /usr/local -xzf golang.tar.gz \
     && rm golang.tar.gz
