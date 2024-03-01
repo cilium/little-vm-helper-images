611dc51dd118ced9c18676f625e01f19608130ab
Mahe Tardy <mahe.tardy@gmail.com>
2024-03-01 14:31:25 +0000

    bootstrap: add arm64 support in scripts
    This mostly uses `dpkg --print-architecture` instead of hardcoding amd64
    everywhere to replace with arm64 when suited.
    
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

--- _data/bootstrap/golang.sh
+++ _data/bootstrap/golang.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 
 # renovate: datasource=golang-version depName=go
 GOLANG_VERSION=1.21.4
-GOLANG_DOWNLOAD_URL=https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz
+GOLANG_DOWNLOAD_URL=https://golang.org/dl/go$GOLANG_VERSION.linux-$(dpkg --print-architecture).tar.gz
 
 curl -fsSL "$GOLANG_DOWNLOAD_URL" -o golang.tar.gz \
     && tar -C /usr/local -xzf golang.tar.gz \
