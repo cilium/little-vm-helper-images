3d2e79eaab12c4203ec2423ce2dfa06683401cc2
Mahé <mahe.tardy@gmail.com>
2024-03-01 15:23:12 +0100

    fstab: add `nofail` option to avoid emergency mode on boot
    If you don't provide the `--host-mount` path, `systemd` starts the
    emergency mode. We still see the error from `host.mount` in the
    `systemd` log but at least it doesn't fail to start.
    
    This induced me into thinking there was something wrong with my kernel
    or image while it just that I didn't gave a `--host-mount` value.
    
    Signed-off-by: Mahé <mahe.tardy@gmail.com>

--- _data/bootstrap/fstab.sh
+++ _data/bootstrap/fstab.sh
@@ -8,7 +8,7 @@ mkdir /host
 config_path="/etc/fstab"
 
 cat > "$config_path" <<EOF
-host_mount  /host  9p  trans=virtio,rw 0  0
+host_mount  /host  9p  trans=virtio,rw,nofail 0  0
 /dev/root   /   ext4    errors=remount-ro   0   1
 
 EOF
