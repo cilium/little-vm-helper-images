6dc0e29d6daebd2b0b2399e76d66f17579edc48c
Mahe Tardy <mahe.tardy@gmail.com>
2024-03-04 15:00:25 +0100

    bootstrap/deb-k8s: use community owned package repositories
    The Google owned repository to install Kubernetes binary are considered
    deprecated and were failing during the installation process: see
    https://kubernetes.io/blog/2023/08/15/pkgs-k8s-io-introduction/
    
    kubectl can still be installed via package managers but I thought it
    would be simpler to just use curl to download the binary and update the
    version with renovate.
    
    Signed-off-by: Mahe Tardy <mahe.tardy@gmail.com>

--- _data/bootstrap/deb-k8s.sh
+++ _data/bootstrap/deb-k8s.sh
@@ -3,10 +3,17 @@ set -euxo pipefail
 
 . /etc/profile
 
-mkdir -p -m 755 /etc/apt/keyrings
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# renovate: datasource=github-releases depName=kubernetes/kubernetes
+KUBECTL_VERSION=1.29.2
+ARCH=$(dpkg --print-architecture)
 
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
+curl -LO "https://dl.k8s.io/release/v$KUBECTL_VERSION/bin/linux/$ARCH/kubectl"
 
-apt-get update --quiet && apt-get install --quiet --yes --no-install-recommends \
-    kubectl
+# validate the binary
+curl -LO "https://dl.k8s.io/release/v$KUBECTL_VERSION/bin/linux/$ARCH/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# remove temp files
+rm kubectl kubectl.sha256
